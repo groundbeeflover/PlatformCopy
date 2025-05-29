d5f1cce8b54da23742bb811064d52d840eee8699
Dimi <johnnnathan@proton.me>
2025-05-29 19:06:56 +0200

    FAQ

--- src/app/(Pages)/FAQ/page.tsx
+++ src/app/(Pages)/FAQ/page.tsx
@@ -28,15 +28,23 @@ export default FAQ
 const FAQDATA = [
     {
         "question" : "What is maasec?",
-        "answer" : "Maasec is an ACM student chapter from maastricht university interested in all things software, but with a focus on cybersecurity."
+        "answer" : "MaaSec is the only ACM Student Chapter in the Netherlands, founded at Maastricht University. It creates opportunities for students to engage with cybersecurity through workshops, open-source development, and international competitions. Rooted in both academic and applied perspectives, MaaSec supports students in exploring the field and contributing to meaningful, student-led initiatives."
     },
     {
         "question" : "What does maasec do?",
-        "answer" : "We organize and participate in hackathons, participate in CTF events, help students work on cybersecurity related projects and organize events where students can learn about various things in cybersecurity."
+        "answer" : "We organize workshops on reverse engineering, safe coding, and cloud security, and host expert lectures to share cutting-edge knowledge. We represent Maastricht University in CTF competitions and run training sessions for students. We offer free security consulting to startups, support student-led research with mentorship and resources, and publish open-source tools. We also take part in national cybersecurity exercises and represent the university at hackathons and specialized events."
+    },
+    {
+        "question" : "What is MaaSec’s mission and what values does it stand for?",
+        "answer" : "MaaSec is dedicated to hands-on learning, open collaboration, and real-world impact. Our mission is to bridge the gap between academic research and practical cybersecurity application. We provide students with tools, challenges, and mentorship to help them grow into ethical hackers, builders, and future cybersecurity leaders. By combining theory with practice, we prepare students to tackle real-world security challenges with confidence and integrity."
+    },
+    {
+        "question" : "Do I need to be an expert to join or take part in MaaSec activities?",
+        "answer" : "Not at all! You don't need any prior experience--just curiosity and motivation. We welcome learners at all levels and provide guidance and resources to help you grow"
     },
     {
         "question" : "How can I join?",
-        "answer" : "You can shoot us an email, (see footer) join our discord (link on home page) and contact one of the founding members!"
+        "answer" : "You can shoot us an email (see footer) join our discord (link on home page) or join our WhatsApp group!"
     }, 
 
 ]
