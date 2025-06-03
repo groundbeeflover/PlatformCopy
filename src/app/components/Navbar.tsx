cd2be1adaf566c011b2eaaa7d80b12c9a0b0f1ad
groundbeeflover <gevegg22@gmail.com>
2025-06-03 21:08:49 +0200

    FIX: made navbar toggle with a button on mobile

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,34 +1,59 @@
+'use client'
+
 import React from 'react'
 import Image from "next/image";
 import Link from "next/link";
+import { useRef } from 'react'
+
+
 
 const navItems = [
-    { label: "Our Events", id: "/Platform/Calendar"},
-    { label: "FAQ", id: "/Platform/FAQ"},
-    { label: "Blog", id: "/Platform/Blog"},
-    { label: "Announcements", id: "/Platform/Announcements"}
+    { label: "Our Events", id: "/Platform/Calendar" },
+    { label: "FAQ", id: "/Platform/FAQ" },
+    { label: "Blog", id: "/Platform/Blog" },
+    { label: "Announcements", id: "/Platform/Announcements" }
 ];
 
 export default function Navbar() {
+    const imgRef = useRef<HTMLImageElement>(null);
+    const menuRef = useRef<HTMLDivElement>(null);
+
+    const toggleIcon = () => {
+        if (!imgRef.current) return;
+        const isMenu = imgRef.current.src.includes('menu.svg');
+        imgRef.current.src = isMenu ? '/Platform/x.svg' : '/Platform/menu.svg';
+
+
+        if (menuRef.current) {
+            const isHidden = menuRef.current.classList.contains('hidden');
+            if (isHidden) {
+                menuRef.current.classList.remove('hidden');
+                menuRef.current.classList.add('flex');
+            } else {
+                menuRef.current.classList.remove('flex');
+                menuRef.current.classList.add('hidden');
+            }
+        }
+    };
+
     return (
         <header className="w-full z-50 transition-all duration-300 bg-white shadow-md">
             <nav className="container mx-auto px-4 py-4 flex justify-between items-center">
                 <div className="flex items-center gap-2">
-                    <Link href="/">
-                        <Image 
-                        src="/Platform/acm.svg" 
-                        alt="ACM logo" 
-                        width={32} 
-                        height={32} 
-                        className='md:w-16 md:h-16' />
-                        <span className="font-archivo font-bold text-md md:text-lg lg:text-xl text-black">
-                            at Maastricht University
+                    <Link href="/" className='flex flex-row gap-2'>
+                        <Image
+                            src="/Platform/acm.svg"
+                            alt="ACM logo"
+                            width={32}
+                            height={32}
+                            className='md:w-24 md:h-24' />
+                        <span className="font-archivo font-bold text-md md:text-lg lg:text-xl text-black py-0.5 md:py-8">
+                            [at Maastricht University]
                         </span>
                     </Link>
                 </div>
-
-                <div className="flex flex-col md:flex-row gap-4 md:gap-8 font-archivo">    
-                {/* <div className="flex gap-4 md:gap-8 font-archivo"> */}
+                <div className="flex-row hidden md:flex gap-4 md:gap-8 font-archivo">
+                    {/* <div className="flex gap-4 md:gap-8 font-archivo"> */}
                     {navItems.map((item) => (
                         <a
                             key={item.id}
@@ -47,7 +72,34 @@ export default function Navbar() {
                         </a>
                     ))}
                 </div>
+                <div className="md:hidden">
+                    <button onClick={toggleIcon}>
+                        <Image
+                            src="/Platform/menu.svg"
+                            ref={imgRef}
+                            alt='menu'
+                            width={32} height={32} />
+                    </button>
+                </div>
             </nav>
+
+            <div
+                ref={menuRef}
+                className="hidden flex-col md:hidden px-4 pb-4 gap-4 font-archivo"
+            >
+                {navItems.map((item) => (
+                    <a
+                        key={item.id}
+                        href={item.id}
+                        className="text-gray-700 text-md border-b w-fit"
+                    >
+                        {item.label}
+                    </a>
+                ))}
+            </div>
+
+
+
         </header>
     );
 }
