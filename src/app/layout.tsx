d4823a3c079d054a38dda9f16d87635eee2d7bff
groundbeeflover <gevegg22@gmail.com>
2025-07-06 14:25:27 +0200

    update cache

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { usePathname } from 'next/navigation';
 export default function RootLayout({ children }: { children: ReactNode }) {
   const pathname = usePathname();
 
+  
   return (
     <html lang="en">
       <body
